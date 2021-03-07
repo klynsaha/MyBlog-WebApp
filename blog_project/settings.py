02744c8e32ef1108d11af7f4e3b243a872d9d182
Kalyan Saha <klynsaha@gmail.com>
2021-03-07 17:32:24 +0530

    added django-heroku

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -152,3 +153,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
